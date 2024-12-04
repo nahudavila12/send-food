596c6eb7d8ce84ace774f93698c6e217c4d658d5
nahudavila12 <nahu.davila@gmail.com>
2024-12-03 23:30:54 -0300

    Esta lista la persistencia

--- src/redux/slices/tableSlice.ts
+++ src/redux/slices/tableSlice.ts
@@ -1,46 +1,42 @@
-"use client"
-import { ITable, ITableState } from '@/interfaces/interfaces';
+// src/store/tablesSlice.ts
+
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 
+type TableStatus = 'disponible' | 'reservada' | 'ocupada';
+
+type Table = {
+  id: string;
+  tableNumber: number;
+  x: number;
+  y: number;
+  status: TableStatus;
+};
 
 interface TablesState {
-  tables: ITable[];
-  loading: boolean;
-  error: string | null;
+  tables: Table[];
 }
 
 const initialState: TablesState = {
   tables: [],
-  loading: false,
-  error: null,
 };
 
 const tablesSlice = createSlice({
   name: 'tables',
   initialState,
   reducers: {
-    fetchTablesRequest(state) {
-      state.loading = true;
-    },
-    fetchTablesSuccess(state, action: PayloadAction<ITable[]>) {
-      state.loading = false;
+    setTables: (state, action: PayloadAction<Table[]>) => {
       state.tables = action.payload;
-      state.error = null;
     },
-    fetchTablesFailure(state, action: PayloadAction<string>) {
-      state.loading = false;
-      state.error = action.payload;
-    },
-    updateTableStatus(state, action: PayloadAction<{ tableNumber: number; status: ITableState }>) {
-      const { tableNumber, status } = action.payload;
-      const table = state.tables.find(t => t.tableNumber === tableNumber);
+    changeTableStatus: (state, action: PayloadAction<{ tableNumber: number; newStatus: TableStatus }>) => {
+      const { tableNumber, newStatus } = action.payload;
+      const table = state.tables.find((table) => table.tableNumber === tableNumber);
       if (table) {
-        table.status = status;
+        table.status = newStatus;
       }
     },
   },
 });
 
-export const { fetchTablesRequest, fetchTablesSuccess, fetchTablesFailure, updateTableStatus } = tablesSlice.actions;
+export const { setTables, changeTableStatus } = tablesSlice.actions;
 
 export default tablesSlice.reducer;
